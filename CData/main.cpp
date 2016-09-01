ba428a129463dab34441f41e5e30a84ec7800c36
tla001 <1414419047@qq.com>
2016-09-01 15:12:17 +0800

    hello

---
+++ CData/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016Äê9ÔÂ1ÈÕ
+ *      Author: Administrator
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"hello"<<endl;
+	return 0;
+}
+
+
+
